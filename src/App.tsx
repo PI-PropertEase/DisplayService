7179a13f00c09eec8b94c5e34bd453ae57214ec1
Diana <dianarrmiranda@gmail.com>
2024-03-13 13:59:31 +0000

    PTSE-17 added dockerfile

--- src/App.tsx
+++ src/App.tsx
@@ -25,7 +25,6 @@ function App() {
         </svg>
         <span>New software update available.</span>
       </div>
-      <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -34,9 +33,6 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
