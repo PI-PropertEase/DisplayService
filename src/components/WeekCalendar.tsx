dfd5f65d54ae339127729b7bc3ef106a134e3f4d
Ricardo Quintaneiro <97852246+ricardoquintaneiro@users.noreply.github.com>
2024-03-15 20:01:09 +0000

    PTSE-31 Added some events

--- src/components/WeekCalendar.tsx
+++ src/components/WeekCalendar.tsx
@@ -12,7 +12,30 @@ export default function WeekCalendar() {
       }}
       height={"100%"}
       buttonIcons={{prev: 'chevron-left', next: 'chevron-right'}}
-      
+      events={
+        [
+          {
+            title: "Hotel 1 - Room 4",
+            start: "2024-03-14",
+            end: "2024-03-15"
+          },
+          {
+            title: "Alojamento Local 1",
+            start: "2024-03-13",
+            end: "2024-03-16"
+          },
+          {
+            title: "Alojamento local 3",
+            start: "2024-03-12",
+            end: "2024-03-17"
+          },
+          {
+            title: "Hotel 2 - Room 2",
+            start: "2024-03-11",
+            end: "2024-03-12"
+          }
+        ]
+      } 
     />
   );
 }
