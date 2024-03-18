c6340264aa4a7a3ff1c0cfd294304b0695a7a422
notjoao1 <joao.dourado1@ua.pt>
2024-03-18 17:09:44 +0000

    PTSE-34 Property List Page: working pagination + fixed buttons to bottom

--- src/components/PropertyTable.tsx
+++ src/components/PropertyTable.tsx
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import { IProperty } from "./PropertyListDashboard";
 import PropertyListBadge from "./PropertyListBadge";
 import { BsBoxArrowUpRight } from "react-icons/bs";
 import { FaArrowLeft, FaArrowRight, FaRegTrashAlt } from "react-icons/fa";
+import {
+  MdKeyboardDoubleArrowLeft,
+  MdKeyboardDoubleArrowRight,
+} from "react-icons/md";
 
 interface IPropertyTableProps {
   propertyList: IProperty[];
@@ -11,11 +15,31 @@ interface IPropertyTableProps {
 const PropertyTable: React.FC<IPropertyTableProps> = (
   props: IPropertyTableProps
 ) => {
+  const PAGE_SIZE = 10;
+
+  const [paginationNumber, setPaginationNumber] = useState<number>(1);
+
+  const numberOfPages = Math.ceil(props.propertyList.length / PAGE_SIZE);
+
+  let paginationArray: number[] = []; // [1,2, ..., n] where n = number of pages
+
+  for (let i = 1; i <= numberOfPages; i++) {
+    paginationArray.push(i);
+  }
+
+  // only show buttons for pages whose number is within 2 of current page
+  // example: current page is 2 -> only show buttons for pages [1, 2, 3, 4]
+  paginationArray = paginationArray.filter(
+    (n) => !(Math.abs(paginationNumber - n) > 2)
+  );
+
   return (
-    <div className="overflow-auto">
+    <div className="overflow-auto h-full relative">
       <div className="table-cell h-[4rem] pl-6 align-middle text-xl">
         Properties
-        <span className="ml-3 badge text-sm bg-orange-300">100 Properties</span>
+        <span className="ml-3 badge text-[0.75rem] bg-[#FEEBE2] text-[#FDA882] dark:bg-orange-900 dark:text-[#FEEBE2] border-none">
+          100 Properties
+        </span>
       </div>
       <table className="table">
         {/* head */}
@@ -37,70 +61,107 @@ const PropertyTable: React.FC<IPropertyTableProps> = (
         </thead>
         <tbody>
           {/* row 1 */}
-          {props.propertyList.map((property: IProperty) => {
-            return (
-              <tr key={property.id}>
-                <th>
-                  <label>
-                    <input type="checkbox" className="checkbox" />
-                  </label>
-                </th>
-                <td>
-                  <div className="flex items-center gap-3">
-                    <div>
-                      <div className="font-bold">{property.name}</div>
-                      <div className="text-sm opacity-50">
-                        {property.address}
+          {props.propertyList
+            .slice(
+              PAGE_SIZE * (paginationNumber - 1),
+              PAGE_SIZE * paginationNumber
+            )
+            .map((property: IProperty) => {
+              return (
+                <tr key={property.id}>
+                  <th>
+                    <label>
+                      <input type="checkbox" className="checkbox" />
+                    </label>
+                  </th>
+                  <td>
+                    <div className="flex items-center gap-3">
+                      <div>
+                        <div className="font-bold">{property.name}</div>
+                        <div className="text-sm opacity-50">
+                          {property.address}
+                        </div>
                       </div>
                     </div>
-                  </div>
-                </td>
-                <td>
-                  <PropertyListBadge text={property.status} />
-                </td>
-                <td className="text-center">
-                  {property.arrival.toLocaleString()}
-                </td>
-                <td className="text-center">
-                  {property.departure.toLocaleString()}
-                </td>
-                <td className="text-center">{property.price}€</td>
-                <td>
-                  <a href="">
-                    <FaRegTrashAlt />
-                  </a>
-                </td>
-                <td>
-                  <a href="">
-                    <BsBoxArrowUpRight />
-                  </a>
-                </td>
-              </tr>
-            );
-          })}
+                  </td>
+                  <td>
+                    <PropertyListBadge text={property.status} />
+                  </td>
+                  <td className="text-center">
+                    {property.arrival.toLocaleString()}
+                  </td>
+                  <td className="text-center">
+                    {property.departure.toLocaleString()}
+                  </td>
+                  <td className="text-center">{property.price}€</td>
+                  <td>
+                    <a href="">
+                      <FaRegTrashAlt />
+                    </a>
+                  </td>
+                  <td>
+                    <a href="">
+                      <BsBoxArrowUpRight />
+                    </a>
+                  </td>
+                </tr>
+              );
+            })}
         </tbody>
       </table>
-      <div className="w-full pl-6 pr-6">
+      {/* Table Footer */}
+      <div className="w-full pt-4 pl-6 pr-6 absolute bottom-10">
         <div className="flex">
-          <button className="btn btn-outline flex-1">
+          <button
+            className="btn btn-outline flex-1"
+            onClick={() => {
+              if (paginationNumber > 1)
+                setPaginationNumber(paginationNumber - 1);
+            }}
+          >
             <FaArrowLeft /> Previous
           </button>
-          {/* Pagination section*/}
           <div className="join flex-[16] justify-center content-center">
-            <button className="join-item btn bg-white dark:bg-transparent">
-              1
+            <button
+              className={`join-item btn bg-white dark:bg-transparent ${
+                paginationNumber === 1 ? "btn-disabled" : ""
+              }`}
+              onClick={() => setPaginationNumber(1)}
+            >
+              {" "}
+              <MdKeyboardDoubleArrowLeft />
             </button>
-            <button className="join-item btn bg-secondary border-none text-primary">
-              2
-            </button>
-            <button className="join-item btn bg-white dark:bg-transparent">
-              3
-            </button>
-            <button className="join-item btn bg-white dark:bg-transparent">
-              4
+            {paginationArray.map((num) => {
+              return (
+                <button
+                  key={num}
+                  className={`join-item btn  ${
+                    paginationNumber === num
+                      ? "bg-secondary dark:bg-orange-800"
+                      : "bg-white dark:bg-transparent"
+                  }`}
+                  onClick={() => setPaginationNumber(num)}
+                >
+                  {num}
+                </button>
+              );
+            })}
+            <button
+              className={`join-item btn bg-white dark:bg-transparent ${
+                paginationNumber === numberOfPages ? "btn-disabled" : ""
+              }`}
+              onClick={() => setPaginationNumber(numberOfPages)}
+            >
+              <MdKeyboardDoubleArrowRight />
             </button>
           </div>
-          <button className="btn btn-outline flex-1">
+          <button
+            className="btn btn-outline flex-1"
+            onClick={() => {
+              if (paginationNumber < numberOfPages)
+                setPaginationNumber(paginationNumber + 1);
+            }}
+          >
             Next <FaArrowRight />
           </button>
         </div>
