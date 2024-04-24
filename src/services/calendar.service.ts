dfbfcd4afc8e1488b48e6c031201490ad1a60a73
Ricardo Quintaneiro <97852246+ricardoquintaneiro@users.noreply.github.com>
2024-04-24 16:04:00 +0100

    PTSE-73 better date parsing

--- src/services/calendar.service.ts
+++ src/services/calendar.service.ts
@@ -3,6 +3,12 @@ import { IReservation } from "../types/ReservationType"
 
 const URL = "http://localhost/api/CalendarService"
 
+const parseISODateWithoutTimezoneOffset = (dateString: string) => {
+  const date = new Date(dateString)
+  const timezoneOffset = date.getTimezoneOffset() * 60000
+  return new Date(date.getTime() - timezoneOffset)
+}
+
 export const fetchReservations = async (authHeader: string) => {
   const res = await axios.get<IReservation[]>(`${URL}/reservations`, {
     headers: {
@@ -14,26 +20,8 @@ export const fetchReservations = async (authHeader: string) => {
   // Date strings are in the format "YYYY-MM-DDTHH:MM:SS" and need to be converted to Date objects with the correct time zone
   const reservations: IReservation[] = res.data.map((r: IReservation) => ({
     ...r,
-    begin_datetime: new Date(
-      Date.UTC(
-        parseInt(r.begin_datetime.slice(0, 4)), // Year
-        parseInt(r.begin_datetime.slice(5, 7)) - 1, // Month (months are zero-based)
-        parseInt(r.begin_datetime.slice(8, 10)), // Day
-        parseInt(r.begin_datetime.slice(11, 13)), // Hours
-        parseInt(r.begin_datetime.slice(14, 16)), // Minutes
-        parseInt(r.begin_datetime.slice(17, 19)) // Seconds
-      )
-    ),
-    end_datetime: new Date(
-      Date.UTC(
-        parseInt(r.end_datetime.slice(0, 4)), // Year
-        parseInt(r.end_datetime.slice(5, 7)) - 1, // Month (months are zero-based)
-        parseInt(r.end_datetime.slice(8, 10)), // Day
-        parseInt(r.end_datetime.slice(11, 13)), // Hours
-        parseInt(r.end_datetime.slice(14, 16)), // Minutes
-        parseInt(r.end_datetime.slice(17, 19)) // Seconds
-      )
-),
+    begin_datetime: parseISODateWithoutTimezoneOffset(r.begin_datetime),
+    end_datetime: parseISODateWithoutTimezoneOffset(r.end_datetime),
   }))
 
   return reservations
