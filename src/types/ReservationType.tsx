e2b717ba9f9ef170f28c09f76d104735782aa0cc
notjoao1 <joao.dourado1@ua.pt>
2024-04-27 18:54:54 +0100

    fix: updated reservation type to match new schema from CalendarService

--- src/types/ReservationType.tsx
+++ src/types/ReservationType.tsx
@@ -1,19 +1,20 @@
-import { ServiceEnum } from "./UserType";
+import { ServiceEnum } from "./UserType"
 
 export interface IReservation {
-  id: number;
-  property_id: number;
-  status: "pending" | "confirm";
-  begin_datetime: Date;
-  end_datetime: Date;
-  service: ServiceEnum;
-  cost: number;
-  client_phone: string;
-  client_name: string;
-  client_email: string;
+  id: number
+  property_id: number
+  reservation_status: ReservationStatus
+  begin_datetime: Date
+  end_datetime: Date
+  service: ServiceEnum
+  cost: number
+  client_phone: string
+  client_name: string
+  client_email: string
 }
 
 export enum ReservationStatus {
-  ONGOING = "OnGoing",
-  PENDING = "Pending"
+  CONFIRMED = "confirmed",
+  PENDING = "pending",
+  CANCELED = "canceled",
 }
