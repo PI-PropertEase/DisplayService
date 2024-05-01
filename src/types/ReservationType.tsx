ac3f65de59cfc9f40e8fed16a6c05462c4c8a5a2
notjoao1 <joao.dourado1@ua.pt>
2024-05-01 22:43:21 +0100

    PTSE-75 added cleaning/maintenance types and refactored types

--- src/types/ReservationType.tsx
+++ src/types/ReservationType.tsx
@@ -1,18 +1,42 @@
 import { ServiceEnum } from "./UserType"
 
+// Generic Event type
 export interface IEvent {
-  id: number
+  id?: number
   property_id: number
   owner_email: string
   begin_datetime: Date
   end_datetime: Date
-  type: string
+  type: IEventType
   service?: ServiceEnum
 }
 
+export enum IEventType {
+  RESERVATION = "reservation",
+  CLEANING = "cleaning",
+  MAINTENANCE = "maintenance",
+}
+
+export interface IReservation extends IEvent {
+  reservation_status: ReservationStatus
+  client_email: string
+  client_name: string
+  client_phone: string
+  cost: number
+}
 
 export enum ReservationStatus {
   CONFIRMED = "confirmed",
   PENDING = "pending",
   CANCELED = "canceled",
+}
+
+// No attributes (for now)
+export interface ICleaning extends IEvent {
+
+}
+
+// No attributes (for now)
+export interface IMaintenance extends IEvent {
+  
 }
