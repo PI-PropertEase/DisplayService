9468208a8906dea0c44d116645401297e682646a
notjoao1 <joao.dourado1@ua.pt>
2024-05-16 22:46:47 +0100

    PTSE-117 showing services in table and added button to filter by service - gotta fix CSS 💀

--- src/components/PropertyTable.tsx
+++ src/components/PropertyTable.tsx
@@ -40,10 +40,23 @@ const PropertyTable = () => {
   // example: current page is 2 -> only show buttons for pages [1, 2, 3, 4]
   paginationArray = paginationArray.filter((n) => !(Math.abs(paginationNumber - n) > 2))
 
+  if (propertyData.length === 0) {
+    return (
+      <div className="flex justify-center items-center flex-col w-[100%] h-[100%] text-center text-4xl gap-4">
+        You do not have any properties imported into our system.<br/>Connect to a supported external service you have properties listed in!
+        <Link to={"/integrations"}>
+          <button className="btn btn-primary w-20">
+            Connect now
+          </button>
+        </Link> 
+      </div>
+    )
+  }
+
   return (
     <div className="overflow-auto h-full">
-      <div className="flex flex-column items-center justify-center pt-1">
-        <div className="table-cell h-[4rem] pl-6 text-xl text-center align-bottom">
+      <div className="h-[5rem] flex flex-row items-center justify-center pt-1">
+        <div className="pl-6 text-xl text-center">
           Properties
           <span className="ml-3 badge text-[0.75rem] bg-secondary text-[#FDA882] dark:bg-orange-900 dark:text-secondary border-none">
             {propertyList?.length ?? 0} Properties
@@ -65,12 +78,10 @@ const PropertyTable = () => {
         <thead className="bg-secondary dark:bg-[#242424] text-black dark:text-white">
           <tr className="max-[760px]:block max-[760px]:clip-out">
             <th>
-              <label>
-                <input type="checkbox" className="checkbox" />
-              </label>
             </th>
             <th>Name and Address</th>
             <th>Status</th>
+            <th className="text-center">From services</th>
             <th className="text-center">Arrival/Upcoming Arrival</th>
             <th className="text-center">Departure/Upcoming Departure</th>
             <th className="text-center">Current Price</th>
@@ -89,9 +100,6 @@ const PropertyTable = () => {
                   className="max-[760px]:block max-[760px]:border-[#eee] max-[760px]:dark:border-[#223] max-[760px]:border-t-8 max-[760px]:border-b-0 max-[760px]:mb-2 max-[760px]:p-2"
                 >
                   <th className="max-[760px]:clip-out">
-                    <label>
-                      <input type="checkbox" className="checkbox" />
-                    </label>
                   </th>
                   <td
                     data-label="Address"
@@ -113,6 +121,13 @@ const PropertyTable = () => {
                     <PropertyListBadge text={property.status} />
                   </td>
                   <td
+                    className="text-center max-[760px]:block max-[760px]:text-right max-[760px]:before:content-datalabel max-[760px]:border-b-[1px] max-[760px]:border-[#eee] max-[760px]:dark:border-[#223]"
+                    data-label="Services"
+                  >
+                    {/* capitalize and then join with comma */}
+                    {property.services.map(s => s.charAt(0).toUpperCase() + s.slice(1)).join(", ")}
+                  </td>
+                  <td
                     data-label="Arrival"
                     className="text-center max-[760px]:block max-[760px]:text-right max-[760px]:before:content-datalabel max-[760px]:border-b-[1px] max-[760px]:border-[#eee] max-[760px]:dark:border-[#223]"
                   >
@@ -129,14 +144,6 @@ const PropertyTable = () => {
                     className="text-center max-[760px]:block max-[760px]:text-right max-[760px]:before:content-datalabel max-[760px]:border-b-[1px] max-[760px]:border-[#eee] max-[760px]:dark:border-[#223]"
                   >
                     {property.price}€
-                  </td>
-                  <td
-                    data-label="Delete"
-                    className="text-center max-[760px]:flex max-[760px]:before:content-datalabel max-[760px]:border-b-[1px] max-[760px]:border-[#eee] max-[760px]:dark:border-[#223]"
-                  >
-                    <a className="max-[760px]:ml-auto">
-                      <FaRegTrashAlt />
-                    </a>
                   </td>
                   <td
                     data-label="More Details"
